82efaafefc219a56ea2edfaccecc7f9151c626bd
Rah1m03 <rahimgazizov2003@gmail.com>
2026-01-30 17:10:40 +0600

    модель студента

---
+++ Models/Students.cs
@@ -0,0 +1,11 @@
+﻿namespace InformationSystemOfASchoolIducationalPortal.Models
+{
+    public class Students
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string UserId { get; set; } = null!;
+        public Users User { get; set; } = null!;
+        public string ClassId { get; set; } = null!;
+        public Class Class { get; set; } = null!;
+    }
+}
