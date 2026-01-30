327b5636b05ddc6e6afe33196174ebaee3e1f26b
Rah1m03 <rahimgazizov2003@gmail.com>
2026-01-30 16:58:04 +0600

    исправление функции и мелкие исправление в методах

--- BissnessLogicUser/CRUDUser.cs
+++ BissnessLogicUser/CRUDUser.cs
@@ -18,56 +18,88 @@ namespace InformationSystemOfASchoolIducationalPortal.BissnessLogicUser
         }
         public async Task<OperationResult> AddUser(CreateUser createUser)
         {
-            var user = new Users
+            try
             {
-                FullName = createUser.FullName,
-                UserName = createUser.Login,
-                BirthDate = createUser.BirthDate,
-            };
-            var result = await _users.CreateAsync(user, createUser.Password);
-            if (!result.Succeeded)
-                return Errors(result);
-            var addRole = await _users.AddToRoleAsync(user, createUser.Role);
-            return Errors(addRole);
+                var user = new Users
+                {
+                    FullName = createUser.FullName,
+                    UserName = createUser.Login,
+                    BirthDate = createUser.BirthDate,
+                };
+                var result = await _users.CreateAsync(user, createUser.Password);
+                if (!result.Succeeded)
+                    return Errors(result);
+                var addRole = await _users.AddToRoleAsync(user, createUser.Role);
+                if (!addRole.Succeeded)
+                    return Errors(addRole);
+                return OperationResult.Ok();
+            }
+            catch (Exception ex)
+            {
+                return OperationResult.Fail(ex.Message);
+            }
         }
         public async Task<OperationResult> Delete(string login)
         {
-            var user = await _users.FindByNameAsync(login);
-            if (user == null)
-                return OperationResult.Fail("Пользователь не найден");
-            var result = await _users.DeleteAsync(user);
-            return Errors(result);
+            try
+            {
+                var user = await _users.FindByNameAsync(login);
+                if (user == null)
+                    return OperationResult.Fail("Пользователь не найден");
+                var result = await _users.DeleteAsync(user);
+                if (!result.Succeeded)
+                    return Errors(result);
+                return OperationResult.Ok();
+            }
+            catch (Exception ex)
+            {
+                return OperationResult.Fail(ex.Message);
+            }
         }
         public async Task<OperationResult> Edit(CreateUser user)
         {
-            var findUser = await _users.FindByNameAsync(user.Login);
-            if (findUser == null)
-                return OperationResult.Fail("Пользователь не найден");
-            var roles = await _users.GetRolesAsync(findUser);
-            var removeRoles = await _users.RemoveFromRolesAsync(findUser, roles);
-            if (!removeRoles.Succeeded)
-                return Errors(removeRoles);
-            var aadRole = await _users.AddToRoleAsync(findUser, user.Role);
-            if (!aadRole.Succeeded)
-                return Errors(aadRole);
-            findUser.FullName = user.FullName;
-            findUser.UserName = user.Login;
-            findUser.BirthDate = user.BirthDate;
-            var result = await _users.UpdateAsync(findUser);
-            if (!result.Succeeded)
-                return Errors(result);
-            return OperationResult.Ok();
+            try
+            {
+                var findUser = await _users.FindByNameAsync(user.Login);
+                if (findUser == null)
+                    return OperationResult.Fail("Пользователь не найден");
+                var roles = await _users.GetRolesAsync(findUser);
+                var removeRoles = await _users.RemoveFromRolesAsync(findUser, roles);
+                if (!removeRoles.Succeeded)
+                    return Errors(removeRoles);
+                var aadRole = await _users.AddToRoleAsync(findUser, user.Role);
+                if (!aadRole.Succeeded)
+                    return Errors(aadRole);
+                findUser.FullName = user.FullName;
+                findUser.UserName = user.Login;
+                findUser.BirthDate = user.BirthDate;
+                var result = await _users.UpdateAsync(findUser);
+                if (!result.Succeeded)
+                    return Errors(result);
+                return OperationResult.Ok();
+            }
+            catch(Exception ex)
+            {
+                return OperationResult.Fail(ex.Message);
+            }
         }
         public async Task<OperationResult> ResetPassword(string login, string newPassword)
         {
-            var user = await _users.FindByNameAsync(login);
-            if (user == null)
-                return OperationResult.Fail("Пользователь не найден");
-            var token = await _users.GeneratePasswordResetTokenAsync(user);
-            var result = await _users.ResetPasswordAsync(user, token, newPassword);
-            if (!result.Succeeded)
-                return Errors(result);
-            return OperationResult.Ok();
+            try
+            {
+                var user = await _users.FindByNameAsync(login);
+                if (user == null)
+                    return OperationResult.Fail("Пользователь не найден");
+                var token = await _users.GeneratePasswordResetTokenAsync(user);
+                var result = await _users.ResetPasswordAsync(user, token, newPassword);
+                if (!result.Succeeded)
+                    return Errors(result);
+                return OperationResult.Ok();
+            }
+            catch(Exception ex)
+            {
+                return OperationResult.Fail(ex.Message);
+            }
         }
         private OperationResult Errors(IdentityResult result) =>
                  OperationResult.Fail(string.Join(", ", result.Errors.Select(r => r.Description)));
