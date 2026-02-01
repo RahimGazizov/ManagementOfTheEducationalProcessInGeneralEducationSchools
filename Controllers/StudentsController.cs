f7f053ccaa5fa6d554d417cf61a33487013041be
Rah1m03 <rahimgazizov2003@gmail.com>
2026-02-01 14:51:56 +0600

    контроллер для информации о студентах а так же для бизнес логики

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using InformationSystemOfASchoolIducationalPortal.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InformationSystemOfASchoolIducationalPortal.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly AppDbContext _context;
+        public StudentsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var students = await _context.Students.Include(c => c.User).Include(c => c.Class)
+                .ToListAsync();
+            return View(students);
+        }
+    }
+}
