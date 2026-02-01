c7f84f0cffa71e83077133cfc732871401686a63
Rah1m03 <rahimgazizov2003@gmail.com>
2026-02-01 14:45:06 +0600

    добавил свойство classId

--- Models/CreateUser.cs
+++ Models/CreateUser.cs
@@ -3,9 +3,11 @@
     public class CreateUser
     {
         public string FullName { get; set; }
-        public string Login {  get; set; }
+        public string Login { get; set; }
         public string Password { get; set; }
-        public string Role {  get; set; }
+        public string Role { get; set; }
         public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string? ClassId { get; set; }
     }
 }
