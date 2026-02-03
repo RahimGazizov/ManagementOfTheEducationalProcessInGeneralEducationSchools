1fcfe714633c63d52f2fb6d6d1d4d885df2eb098
Rah1m03 <rahimgazizov2003@gmail.com>
2026-02-03 16:12:03 +0600

    новая модель для редоктирования пользователей

---
+++ Models/EditUserDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace InformationSystemOfASchoolIducationalPortal.Models
+{
+    public class EditUserDTO
+    {
+        public string UserId { get; set; }
+        public string FullName { get; set; }
+        public string Login { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Role { get; set; }
+
+        // Для ученика
+        public string? StudentClassId { get; set; }
+        public int? StudentClassNumber { get; set; }
+        public string? StudentClassLetter { get; set; }
+
+        // Для учителя
+        public List<string> TeacherSubjects { get; set; } = new();
+        public List<string> TeacherClassIds { get; set; } = new();
+    }
+}
