5c4c1a63df0511491d2a932831633b4ce5b5f880
Rah1m03 <rahimgazizov2003@gmail.com>
2026-02-04 13:23:58 +0600

    добавил к свойстам разрешение на нулл

--- Models/Students.cs
+++ Models/Students.cs
@@ -3,9 +3,9 @@
     public class Students
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
-        public string UserId { get; set; }
-        public Users User { get; set; }
-        public string ClassId { get; set; } 
-        public Class Class { get; set; }
+        public string UserId { get; set; } = null!;
+        public Users User { get; set; } = null!;
+        public string? ClassId { get; set; } = null!;
+        public Class? Class { get; set; } = null!;
     }
 }
