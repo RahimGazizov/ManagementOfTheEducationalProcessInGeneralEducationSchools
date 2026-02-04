39c480671fdc2cb7ad27137d47d60536e8bafefe
Rah1m03 <rahimgazizov2003@gmail.com>
2026-02-04 13:27:33 +0600

    Сделал Student.Class nullable, добавил миграцию

--- Migrations/20260204070338_MakeStudentClassNullable.Designer.cs
+++ Migrations/20260204070338_MakeStudentClassNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using InformationSystemOfASchoolIducationalPortal.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InformationSystemOfASchoolIducationalPortal.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260204070338_MakeStudentClassNullable")]
+    partial class MakeStudentClassNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.23");
---
+++ Migrations/20260204070338_MakeStudentClassNullable.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InformationSystemOfASchoolIducationalPortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeStudentClassNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Classes_ClassId",
+                table: "Students");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Classes_ClassId1",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_ClassId1",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "ClassId1",
+                table: "Students");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClassId",
+                table: "Students",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Classes_ClassId",
+                table: "Students",
+                column: "ClassId",
+                principalTable: "Classes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Classes_ClassId",
+                table: "Students");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClassId",
+                table: "Students",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClassId1",
+                table: "Students",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_ClassId1",
+                table: "Students",
+                column: "ClassId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Classes_ClassId",
+                table: "Students",
+                column: "ClassId",
+                principalTable: "Classes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Classes_ClassId1",
+                table: "Students",
+                column: "ClassId1",
+                principalTable: "Classes",
+                principalColumn: "Id");
+        }
+    }
+}
