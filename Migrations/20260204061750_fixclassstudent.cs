a8f1cb263afcf23e07d71afd0ea6d0e9167ed3ff
Rah1m03 <rahimgazizov2003@gmail.com>
2026-02-04 12:33:38 +0600

    Исправлена связь Class-Students, добавлены миграции для изменения отношения

---
+++ Migrations/20260204061750_fixclassstudent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InformationSystemOfASchoolIducationalPortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixclassstudent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
