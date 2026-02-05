dbef74734aa56be95ced80d5edff3c60f4a069f4
Rah1m03 <rahimgazizov2003@gmail.com>
2026-02-05 12:49:37 +0600

    модель предмета

---
+++ Models/Subjects.cs
@@ -0,0 +1,9 @@
+﻿namespace InformationSystemOfASchoolIducationalPortal.Models
+{
+    public class Subjects
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; }
+
+    }
+}
