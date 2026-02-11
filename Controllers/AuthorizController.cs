d69636be09d54b4747ac62206c7697b17b4510b0
Rah1m03 <rahimgazizov2003@gmail.com>
2026-02-11 15:06:55 +0600

    контроллер авторизации

---
+++ Controllers/AuthorizController.cs
@@ -0,0 +1,44 @@
+﻿using InformationSystemOfASchoolIducationalPortal.Data;
+using InformationSystemOfASchoolIducationalPortal.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InformationSystemOfASchoolIducationalPortal.Controllers
+{
+    public class AuthorizController : Controller
+    {
+        private readonly UserManager<Users> _users;
+        public AuthorizController(UserManager<Users> users)
+        {
+            _users = users;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(string login, string password)
+        {
+            var user = await _users.FindByNameAsync(login);
+
+            if (user == null)
+            {
+                TempData["Error"] = "Пользователь с таким логином не существует";
+                ViewBag.Login = login;
+                ViewBag.Password = password;
+                return View();
+            }
+            var result = await _users.CheckPasswordAsync(user, password);
+            if (result)
+                return RedirectToAction("Index", "Home");
+            else
+            {
+                TempData["Error"] = "Не верный логин или пароль";
+                ViewBag.Login = login;
+                ViewBag.Password = password;
+                return View();
+            }
+        }
+    }
+}
