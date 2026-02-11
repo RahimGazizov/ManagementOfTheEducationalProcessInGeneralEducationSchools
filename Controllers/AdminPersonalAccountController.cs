03d7fbc32b52976c3d74e6571b3ca1bdff672fa7
Rah1m03 <rahimgazizov2003@gmail.com>
2026-02-11 16:28:45 +0600

    личный кабинет для админа

---
+++ Controllers/AdminPersonalAccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InformationSystemOfASchoolIducationalPortal.Controllers
+{
+    public class AdminPersonalAccountController : Controller
+    {
+        [Authorize(Roles = "Админ")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
