b5c03f95d135c38c9132c4e281dd04ec9fcc416a
Rah1m03 <rahimgazizov2003@gmail.com>
2026-02-11 15:07:31 +0600

    контроллер информации о учителях

---
+++ Controllers/TeachersController.cs
@@ -0,0 +1,20 @@
+﻿using InformationSystemOfASchoolIducationalPortal.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InformationSystemOfASchoolIducationalPortal.Controllers
+{
+    public class TeachersController : Controller
+    {
+        private readonly AppDbContext _context;
+        public TeachersController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var teachers = await _context.Teachers.Include(u => u.User).ToListAsync();
+            return View(teachers);
+        }
+    }
+}
