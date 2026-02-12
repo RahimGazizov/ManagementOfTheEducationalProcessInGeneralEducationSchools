fbf12b2dfe70cc6bcd4d9e5a38a291d4cc2e7792
Rah1m03 <rahimgazizov2003@gmail.com>
2026-02-12 16:36:25 +0600

    бизнес логика для TeacherAssigment

---
+++ BissnessLogicUser/TeachingAssigmentLogic.cs
@@ -0,0 +1,32 @@
+﻿using InformationSystemOfASchoolIducationalPortal.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+namespace InformationSystemOfASchoolIducationalPortal.BissnessLogicUser
+{
+    public class TeachingAssigmentLogic
+    {
+        private readonly AppDbContext _context;
+        public TeachingAssigmentLogic(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<SelectListItem>> GetListTeachers()
+        {
+            var list = await _context.Teachers.Select(t => new SelectListItem
+            {
+                Value = t.Id,
+                Text = t.User.UserName,
+            }).ToListAsync();
+            return list;
+        }
+        public async Task<List<SelectListItem>> GetListSubjects()
+        {
+            var list = await _context.Subjects.Select(s => new SelectListItem
+            {
+                Value = s.Id,
+                Text = s.Name,
+            }).ToListAsync();
+            return list;
+        }
+    }
+}
