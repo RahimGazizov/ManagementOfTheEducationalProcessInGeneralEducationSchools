71ec3d429a10279134605656c7ba8e3566c003b0
Rah1m03 <rahimgazizov2003@gmail.com>
2026-02-12 16:21:50 +0600

    в индексе написал вывод информацию по авторизованному админу а так же логику удаления куки при выходе из программы

--- Controllers/AdminPersonalAccountController.cs
+++ Controllers/AdminPersonalAccountController.cs
@@ -1,14 +1,36 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-
+using InformationSystemOfASchoolIducationalPortal.Models;
+using Microsoft.EntityFrameworkCore;
+using InformationSystemOfASchoolIducationalPortal.Data;
 namespace InformationSystemOfASchoolIducationalPortal.Controllers
 {
+
+    [Authorize(Roles = "Админ")]
     public class AdminPersonalAccountController : Controller
     {
-        [Authorize(Roles = "Админ")]
-        public IActionResult Index()
+        private readonly SignInManager<Users> _signIn;
+        private readonly AppDbContext _context;
+        private readonly UserManager<Users> _userMan;
+        public AdminPersonalAccountController(SignInManager<Users> signInManager,AppDbContext context,
+            UserManager<Users> manager)
         {
-            return View();
+            _signIn = signInManager;
+            _context = context;
+            _userMan = manager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var userId = _userMan.GetUserId(User);
+            var admin = await _context.Admins.Include(u => u.User)
+                .FirstOrDefaultAsync(id => id.UserId == userId);
+            return View(admin);
+        }
+        public async Task<IActionResult> LogOut()
+        {
+            await _signIn.SignOutAsync();
+            return RedirectToAction("Index", "Authoriz");
         }
     }
 }
