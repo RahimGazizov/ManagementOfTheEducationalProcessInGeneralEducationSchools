01144af73132a2e41f320a33d0b1cc9e1c207d76
Rah1m03 <rahimgazizov2003@gmail.com>
2026-02-12 16:39:08 +0600

    модель для таблицы TeacherAssigment

---
+++ Models/TeacherAssigment.cs
@@ -0,0 +1,15 @@
+﻿namespace InformationSystemOfASchoolIducationalPortal.Models
+{
+    public class TeacherAssigment
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string TeacherId { get; set; }
+        public Teachers Teacher { get; set; }
+
+        public string SubjectId { get; set; }
+        public Subjects Subject { get; set; }
+
+        public string ClassId { get; set; }
+        public Class Class { get; set; }
+    }
+}
